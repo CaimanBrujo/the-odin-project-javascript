1f3934ed3930ad8cdaf0c6b5ee740c5a0eb963c5
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-06-10 15:23:50 +0200

    feat(library): toggle form visibility on New Book button click

--- library/script.js
+++ library/script.js
@@ -1,4 +1,13 @@
 const myLibrary = [];
+const newBookbtn = document.querySelector("#new-book-btn");
+const newBookForm = document.querySelector("#form-container")
+
+
+newBookbtn.addEventListener("click", toggleForm);
+
+function toggleForm() {
+    newBookForm.classList.toggle("show");
+}
 
 function Book() {
   // the constructor...
@@ -7,3 +16,4 @@ function Book() {
 function addBookToLibrary() {
   // take params, create a book then store it in the array
 }
+
