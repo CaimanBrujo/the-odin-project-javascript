530d69a1f6a413702637620a2cc7d4e8a4147884
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-06-11 18:09:16 +0200

    feat(tic-tac-toe): implemented checkWinner to detect 3-in-a-row winning combinations

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -53,10 +53,33 @@ const GameController = (() => {
         }
 
         switchPlayer();
-        };
+    };
 
     const checkWinner = () => {
+        const board = Gameboard.getBoard();
+
+        const winningCombos = [
+            [0, 1, 2], // row 1
+            [3, 4, 5], // row 2
+            [6, 7, 8], // row 3
+            [0, 3, 6], // col 1
+            [1, 4, 7], // col 2
+            [2, 5, 8], // col 3
+            [0, 4, 8], // diagonal \
+            [2, 4, 6]  // diagonal /
+        ];
+
+        return winningCombos.some(combo => {
+            const [a, b, c] = combo;
+
+            return (
+                board[a] !== "" &&
+                board[a] === board[b] &&
+                board[a] === board[c]
+                );
+        });
     };
+
 
     const checkTie = () => {
     };
@@ -65,5 +88,5 @@ const GameController = (() => {
     };
 
     return { playRound, resetGame };
-    
+
 })();
